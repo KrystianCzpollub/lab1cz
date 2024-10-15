feb90dccb29b2e2183c2b95d1f036c9dbaf7c179
kcz <czkrystian04@gmail.com>
2024-10-15 10:30:25 +0200

    Initial Commit

---
+++ lab1cz/lab1czApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab1czApp.swift
+//  lab1cz
+//
+//  Created by student on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct lab1czApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
